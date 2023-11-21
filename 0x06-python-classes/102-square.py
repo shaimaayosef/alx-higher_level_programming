e0f7c1054cebf8f99543ecb6d7fd3cf0bb38c4ea
shaimaayosef <shimaa.sayed8888@gmail.com>
2023-11-22 00:48:12 +0200

    c

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,52 +1,115 @@
 #!/usr/bin/python3
-"""Square class defination"""
+"""Defines a class Square"""
 
 
 class Square:
-    """square body"""
+    """
+    Class that defines properties of square by: (based on 3-square.py).
 
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size=0):
-        """square contructor 
-        Args: size: length of a side of Square
+        """Creates new instances of square.
+
+        Args:
+            size: size of the square (1 side).
         """
         self.__size = size
 
+    def area(self):
+        """Calculates the area of square.
+
+        Returns: the current square area.
+        """
+        return self.__size ** 2
+
     @property
     def size(self):
-        """"The propery of size as the length 
-        of a side of Square
-        Raises:
-            TypeError: if size != int
-            ValueErrorr: if size < 0
+        """Returns the size of a square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Property setter for size.
+
+        Args:
+            value (int): size of a square (1 side).
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
-
-    def area(self):
-        """Get the area instance to comparators"""
-        return self.__size * self.__size
-
-    def __le__(self, other):
-        return self.area() <= other.area()
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def __lt__(self, other):
-        return self.area() < other.area()
+        """Rich comparison operator to compare if square area is less
+        than another.
 
-    def __ge__(self, other):
-        return self.area() >= other.area()
+        Args:
+            other (Square): square to compare size to.
 
-    def __ne__(self, other):
-        return self.area() != other.area()
+        Returns: True or false.
+        """
+        return self.__size < other.__size
 
-    def __gt__(self, other):
-        return self.area() > other.area()
+    def __le__(self, other):
+        """Rich comparison operator to compare if square area is less
+        than or equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false.
+        """
+        return self.__size <= other.__size
 
     def __eq__(self, other):
-        return self.area() == other.area()
+        """Rich comparison operator to compare if square area is equal to
+        another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size == other.__size
+
+    def __ne__(self, other):
+        """Rich comparison operator to compare if square area is not
+        equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        """Rich comparison operator to compare if square area is greater
+        than another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        """Rich comparison operator to compare if square area is greater
+        than or equal to another.
+
+        Args:
+            other (Square): square to compare size to.
+
+        Returns: True or false
+        """
+        return self.__size >= other.__size
