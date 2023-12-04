5db693496846539cb02363b61811e05d203538d2
shaimaayosef <shimaa.sayed8888@gmail.com>
2023-12-05 00:08:27 +0200

    b

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,7 +2,6 @@
 """Defines Rectangle module."""
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
     """class body."""
 
@@ -11,6 +10,5 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.__size = size
 
-    def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
-
+        def __str__(self):
+            return "[Square] {}/{}".format(self.__size, self.__size)
